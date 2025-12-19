8c673107376038b80e8d9fe4594d834399e45b61
ison95 <son.isaac95@gmail.com>
2025-12-19 11:57:24 +0900

    finished v1
    - successfully integrated with openai API with key in .env
    - successfully reads json test cases and passes them into openai
    - successfully receives and saves bot responses
    - successfully evaluates max_words invariant
    - successfully ties the pass/fail condition to results

---
+++ chatbots/chat_model_triage_manager.py
@@ -0,0 +1,19 @@
+# Add the other bot managers here
+from . import openai_manager
+
+
+# GENERAL TRIAGE
+def triage_response_from_bot(bot_name: str, model_name: str, prompt: str) -> str:
+    if bot_name == "openai":
+        bot_resp = triage_response_from_openai_models(model_name, prompt)
+        return bot_resp
+    else:
+        raise RuntimeError(f"ERROR: Attempted to triage {bot_name}, but its triage hasn't been implemented yet in"
+                           f"chat_model_triage_manager.py under the triage_response_from_bot function.")
+
+
+# OPENAI MODELS
+def triage_response_from_openai_models(model_name: str, prompt: str):
+    model_client = openai_manager.setup()
+    model_response = openai_manager.prompt_openai(model_client, model_name, prompt)
+    return model_response
--- runner.py
+++ runner.py
@@ -1,17 +1,14 @@
 import json
-import chat_model_triage_manager
+from chatbots import chat_model_triage_manager
+from evals import eval_triage_manager
 
 
-# TODO: Create a function that takes in a test case's contents: ID, Prompt, Invariant. Should return the output.
-def response_from_chatbot(bot: str, model: str, prompt: str) -> str:
-    model_response = chat_model_triage_manager.triage_response_from_bot(bot, model, prompt)
-    return model_response
-
-
-# TODO: Create a function that takes in an output string and determines whether it is "correct".
-def evaluate_response_from_chatbot(response: str, invariant_type: str, *invariant_vars) -> bool:
-    # TODO: Maybe create an invariant file that triages to the pertinent check(s)?
-    return
+# Fetches JSON data for test cases
+def test_case_setup(json_file_name: str) -> dict:
+    with open(json_file_name, "r") as file:
+        data = json.load(file)
+    print(data)
+    return data
 
 
 # Determines the test cases to run and creates a dict for:
@@ -19,17 +16,10 @@ def evaluate_response_from_chatbot(response: str, invariant_type: str, *invarian
 def run_test_cases(test_case_dict: dict) -> dict:
     results_dict = {}
     for t in test_case_dict:
-        resp = response_from_chatbot(t["prompt"], t["bot"], t["model"])
+        resp = response_from_chatbot(t["bot"], t["model"], t["prompt"])
         resp_eval = evaluate_response_from_chatbot(resp, t["invariant_type"], t[t["invariant_type"]])
         results_dict[t["id"]] = resp_eval
     return results_dict
-
-
-# Fetches JSON data for test cases
-def test_case_setup(json_file_name: str) -> dict:
-    with open(json_file_name, "r") as file:
-        data = json.load(file)
-    return data
 
 
 # TODO: Create a function that's responsible for storing the results of the run
@@ -37,8 +27,20 @@ def create_results_file(results_dict: dict):
     return
 
 
+# Receives output from specified bot
+def response_from_chatbot(bot: str, model: str, prompt: str) -> str:
+    model_response = chat_model_triage_manager.triage_response_from_bot(bot, model, prompt)
+    return model_response
+
+
+# Receives bot response and determines whether it is "correct" by specified invariance
+def evaluate_response_from_chatbot(response: str, invariant_type: str, invariant_var) -> bool:
+    resp_eval = eval_triage_manager.evaluate_response(response, invariant_type, invariant_var)
+    return resp_eval
+
+
 if __name__ == "__main__":
-    fetched_tests = test_case_setup("prompts.json")
+    fetched_tests = test_case_setup("test_cases/max_char_test_cases.json")
     eval_results = run_test_cases(fetched_tests)
     print(eval_results)
 
